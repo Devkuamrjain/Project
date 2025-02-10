06020eb3c00a7615079cbf43aac2a8d248c8ea9f
Devkuamrjain <dj2328073@gmail.com>
2025-02-10 18:09:28 +0530

    Add files via upload

---
+++ js/index-umd.js
@@ -0,0 +1,34 @@
+/**
+ * --------------------------------------------------------------------------
+ * Bootstrap index.umd.js
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
+ * --------------------------------------------------------------------------
+ */
+
+import Alert from 'js/alert.js'
+import Button from 'js/button.js'
+import Carousel from 'js/carosel.js'
+import Collapse from 'js/collapse.js'
+import Dropdown from 'js/dropdown.js'
+import Modal from 'js/modal.js'
+import Offcanvas from 'js/off-anvas.js'
+import Popover from 'js/popover.js'
+import ScrollSpy from 'js/scrollspy.js'
+import Tab from 'js/tabs.js'
+import Toast from 'js/toast.js'
+import Tooltip from 'js/tooltip.js'
+
+export default {
+  Alert,
+  Button,
+  Carousel,
+  Collapse,
+  Dropdown,
+  Modal,
+  Offcanvas,
+  Popover,
+  ScrollSpy,
+  Tab,
+  Toast,
+  Tooltip
+}
